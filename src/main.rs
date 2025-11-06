7443bfa796d5b87143e4561b7c49bd97a4b89573
Omar Carey <oac1771@gmail.com>
2025-11-05 20:50:29 -0800

    add css

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use dioxus::prelude::*;
 
+const MAIN_CSS: Asset = asset!("assets/main.css");
+
 fn main() {
     dioxus::launch(App);
 }
@@ -7,6 +9,32 @@ fn main() {
 #[component]
 fn App() -> Element {
     rsx! {
-        div { "Hello, world!" }
+        document::Stylesheet { href: MAIN_CSS }
+        Title {}
+        DogView {}
+    }
+}
+
+#[component]
+fn Title() -> Element {
+    rsx! {
+        div { id: "title",
+            h1 { "Iot" }
+        }
+    }
+}
+
+#[component]
+fn DogView() -> Element {
+    rsx! {
+        div { id: "buttons",
+            button { id: "scan", "scan" }
+        }
+
+        { (0..10).map(|idx| rsx! {
+            div {
+                h1 {"item {idx}"}
+            }
+        })}
     }
 }
